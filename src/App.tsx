e66c7736ad9b8975862db856e49118e336d1c751
fbiayna <ferranbiaynacosta@gmail.com>
2023-03-11 18:02:16 +0100

    Added useLazyQuery and search button for orderID

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,55 @@
-import React from 'react'
-import { useQuery, gql } from '@apollo/client'
+import React, { useState } from 'react'
+import { useLazyQuery, gql } from '@apollo/client'
 import ReactJson from 'react-json-view'
 
-const App = () => {
-  // Schema
+// Schema
 
-  const fetchOrders = gql`
-    query FetchOrders {
-      fetchOrders {
-        assignedTo
-        createdAt
-        customer {
-          address
-          email
+const fetchOrder = gql`
+  query FetchOrder($fetchOrderId: ID!) {
+    fetchOrder(id: $fetchOrderId) {
+      id
+      state
+      updatedAt
+      createdAt
+      assignedTo
+      customer {
+        address
+        email
+        id
+        name
+      }
+      lineItems {
+        id
+        product {
           id
           name
+          price
         }
-        id
-        lineItems {
-          id
-          product {
-            id
-            name
-            price
-          }
-          quantity
-        }
-        state
-        updatedAt
+        quantity
       }
     }
-  `
+  }
+`
 
-  // Query
+const App = () => {
+  // State
 
-  const { loading, error, data } = useQuery(fetchOrders)
+  const [orderIdInput, setOrderIdInput] = useState<string>('')
+  const [getOrder, { loading, error, data }] = useLazyQuery(fetchOrder)
+  const [queryCompleted, setQueryCompleted] = useState<boolean>(false)
+
+  // Actions
+
+  const onOrderIdInputChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOrderIdInput(event.target.value)
+    setQueryCompleted(false)
+  }
+
+  const onFetchOrderSubmitTapped = () => {
+    orderIdInput && getOrder({ variables: { fetchOrderId: orderIdInput } })
+    setQueryCompleted(true)
+    setOrderIdInput('')
+  }
 
   // Render
 
@@ -43,8 +58,16 @@ const App = () => {
 
   return (
     <div>
-      <h1>Fetch Orders</h1>
-      <ReactJson src={data} />
+      <h1>Fetch Order</h1>
+      <input
+        type='text'
+        value={orderIdInput}
+        onChange={onOrderIdInputChanged}
+        placeholder='Enter an orderId'
+      />
+      <button onClick={onFetchOrderSubmitTapped}>Search</button>
+      {!loading && queryCompleted && (!data || !data.fetchOrder) && <p>No data found</p>}
+      {data && data.fetchOrder && <ReactJson src={data.fetchOrder} />}
     </div>
   )
 }
