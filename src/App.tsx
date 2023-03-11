367a8d7a543a391aca33c325b52e0afae1f594a3
fbiayna <ferranbiaynacosta@gmail.com>
2023-03-11 16:45:46 +0100

    Added basic structure and schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useQuery, gql } from '@apollo/client'
+import ReactJson from 'react-json-view'
 
-function App() {
+const App = () => {
+  // Schema
+
+  const fetchOrders = gql`
+    query FetchOrders {
+      fetchOrders {
+        assignedTo
+        createdAt
+        customer {
+          address
+          email
+          id
+          name
+        }
+        id
+        lineItems {
+          id
+          product {
+            id
+            name
+            price
+          }
+          quantity
+        }
+        state
+        updatedAt
+      }
+    }
+  `
+
+  // Query
+
+  const { loading, error, data } = useQuery(fetchOrders)
+
+  // Render
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error : {error.message}</p>
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Fetch Orders</h1>
+      <ReactJson src={data} />
     </div>
   )
 }
